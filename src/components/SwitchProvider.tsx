bbe9a65f651091cb54fc57b7cd01d7b78b850bf1
Glitch <glitch.txs@gmail.com>
2023-04-18 17:43:59 -0300

    force build

--- src/components/SwitchProvider.tsx
+++ src/components/SwitchProvider.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 const SwitchProvider = () => {
     const initMMSDK = web3Store((s)=>s.initMMSDK)
+    
   return (
     <button
     className='py-0.5 px-2 rounded-md hover:bg-gray-800 transition duration-75 border-2' 
